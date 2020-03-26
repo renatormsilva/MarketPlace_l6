5b172c45bfa9234c525c4bf4e0f5ed9d9cf917e7
renatodln123 <renato.brtkm@gmail.com>
2020-03-26 18:58:05 -0300

    mass assgnment e update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+Route::get('/model', function(){
+     // $productList = \App\Product::all(); // select * from products
+    
+    // $user = \App\User::all(); // retorna todos os usuarios
+    // $user = \App\User::create([
+    //     'name' => 'renatinhosenpai',
+    //     'email' => 'renatinhosenpai@gamil.com',
+    //     'password' => bcrypt('12345')
+    // ]);
+    $user = \App\User::find(41);
+    $user->update([
+        'name' => 'novo nome com update'
+    ]);
+    dd($user);
+     return \App\User::all();  
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
